beb992b63e5a368fe35ad67fbfdb58a9573616f6
nenad-dram <nenaddramicanin@gmail.com>
2022-02-20 16:50:10 +0100

    Initial commit - add config-service and discovery-service

---
+++ config-service/src/test/java/com/endyary/samplemicroservices/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endyary.samplemicroservices.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
